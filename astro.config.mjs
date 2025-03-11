074b2b5cd1b6303921158810ec0a1208bc9d203f
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2025-03-11 14:32:28 +0100

    ajout pasge contact a prorpos et info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind({ applyBaseStyles: false })],
   experimental: { svg: true },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
